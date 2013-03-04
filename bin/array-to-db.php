d5ec9cc09288a50725596dff7482c88b74d321cf
Jérémy Huet <jeremy.huet@gmail.com>
2013-03-04 14:09:15 +0100

    Finalizing storing in db.

--- bin/array-to-db.php
+++ bin/array-to-db.php
@@ -16,12 +16,10 @@ use Ololz\Entity;
  * @param \Ololz\Entity\Source              $lolKingSource
  * @param \Ololz\Service\Persist\Mapping    $mappingService
  * @param \Ololz\Service\Persist\Summoner   $summonerService
- * @param \Ololz\Entity\Spell               $smiteSpell
- * @param \Ololz\Entity\Position            $junglerPosition
  *
  * @return \Ololz\Entity\Invocation
  */
-function fillUpInvocation($aInvocationName, $aInvocation, $lolKingSource, $mappingService, $summonerService, $smiteSpell, $junglerPosition)
+function fillUpInvocation($aInvocationName, $aInvocation, $lolKingSource, $mappingService, $summonerService)
 {
     $invocationVals = array('champion', 'kills', 'deaths', 'assists', 'gold', 'minions', 'spells', 'items', 'damageDealt', 'damageReceived', 'healingDone', 'largestMultiKill', 'timeSpentDead', 'turretsDestroyed');
     $invocation = new Entity\Invocation;
@@ -73,14 +71,65 @@ function fillUpInvocation($aInvocationName, $aInvocation, $lolKingSource, $mappi
         }
     }
 
-    if ($invocation->hasSpell($smiteSpell)) {
-        $invocation->setPosition($junglerPosition);
-    } else if ($invocation->getChampion()) {
+    // Default champion position ; "real" position will be guessed later once whole team is there
+    if ($invocation->getChampion()) {
         $invocation->setPosition($invocation->getChampion()->getPosition());
     }
 
-
     return $invocation;
+}
+
+/**
+ * @param \Ololz\Entity\MatchTeam $matchTeam
+ * @param \Ololz\Entity\Position $position
+ */
+function teamHasMoreThanOnePosition($matchTeam, $position) {
+    $ofPosition = array();
+
+    /* @var $invocation \Ololz\Entity\Invocation */
+    foreach ($matchTeam->getInvocations() as $invocation) {
+        if ($invocation->getPosition() == $position) {
+            $ofPosition[] = $invocation;
+        }
+    }
+
+    return count($ofPosition) > 1 ? $ofPosition : false;
+}
+
+/**
+ * @param \Ololz\Entity\MatchTeam   $matchTeam
+ * @param \Ololz\Entity\Spell       $smiteSpell
+ * @param array                     $supportSpells
+ * @param array                     $positionsByKey
+ */
+function guessTeamPositions($matchTeam, $smiteSpell, $supportSpells, $positionsByKey) {
+    $invocations = $matchTeam->getInvocations();
+
+    /* @var $invocation \Ololz\Entity\Invocation */
+    // Easy one, let's get the jungler !
+    foreach ($invocations as $invocation) {
+        if ($invocation->hasSpell($smiteSpell)) {
+            $invocation->setPosition($positionsByKey['jungle']);
+        }
+    }
+
+    // Now, if we have more than 1 player with the same position, we check if
+    // one of them has a "support" spell such has exhaust or heal or
+    // clairvoyance
+    foreach ($positionsByKey as $position) {
+        $ofSamePosition = teamHasMoreThanOnePosition($matchTeam, $position);
+        if ($ofSamePosition) {
+            foreach ($ofSamePosition as $invocation) {
+                foreach ($supportSpells as $supportSpell) {
+                    if ($invocation->hasSpell($supportSpell)) {
+                        // If we found him, there's no other... Hopefully !
+                        $invocation->setPosition($positionsByKey['support']);
+                        break 3;
+                    }
+                }
+            }
+        }
+    }
 }
 
 /* @var $em \Doctrine\ORM\EntityManager */
@@ -91,27 +140,22 @@ $summonerService    = $application->getServiceManager()->get('Ololz\Service\Pers
 $mappingService     = $application->getServiceManager()->get('Ololz\Service\Persist\Mapping');
 /* @var $matchService \Ololz\Service\Persist\Match */
 $matchService       = $application->getServiceManager()->get('Ololz\Service\Persist\Match');
+/* @var $positionService \Ololz\Service\Persist\Match */
+$positionService       = $application->getServiceManager()->get('Ololz\Service\Persist\Position');
+/* @var $spellService \Ololz\Service\Persist\Spell */
+$spellService       = $application->getServiceManager()->get('Ololz\Service\Persist\Spell');
 /* @var $lolKingSource \Ololz\Entity\Source */
 $lolKingSource      = $application->getServiceManager()->get('Ololz\Mapper\Source')->findOneByCode(Entity\Source::CODE_LOLKING);
 /* @var $smiteSpell \Ololz\Entity\Spell */
-$smiteSpell         = $application->getServiceManager()->get('Ololz\Mapper\Spell')->findOneByCode('smite');
-/* @var $junglerPosition \Ololz\Entity\Position */
-$junglerPosition    = $application->getServiceManager()->get('Ololz\Mapper\Position')->findOneByCode('jungle');
+$smiteSpell         = $spellService->getMapper()->findOneByCode('smite');
+/* @var $twistedMap \Ololz\Entity\Map */
+$twistedMap         = $application->getServiceManager()->get('Ololz\Mapper\Map')->findOneByCode('twisted-treeline');
 
+$positionsByKey = $positionService->getMapper()->findWithKey('code', null);
+$supportSpells = $spellService->getMapper()->findBy(array('code' => array('clairvoyance', 'exhaust', 'heal')));
 
 foreach ($stats['matches'] as $aMatch) {
     $match = new Entity\Match;
-
-    // Match type
-    $matchType = $mappingService->getMapper()->findOneOurs($lolKingSource, Entity\Mapping::TYPE_MATCH_TYPE, $aMatch['type']);
-    if ($matchType) {
-        $match->setMatchType($matchType);
-
-        // Match map
-        if ($matchType->getMap()) {
-            $match->setMap($matchType->getMap());
-        }
-    }
 
     // Match date
     $match->setDate(new \DateTime($aMatch['date'] . ' 00:00:00'));
@@ -123,7 +167,7 @@ foreach ($stats['matches'] as $aMatch) {
     $teammateTeam = new Entity\MatchTeam;
     $teammateTeam->setMatch($match);
     foreach ($aMatch['teammates'] as $aTeammateName => $aTeammate) {
-        $teammate = fillUpInvocation($aTeammateName, $aTeammate, $lolKingSource, $mappingService, $summonerService, $smiteSpell, $junglerPosition);
+        $teammate = fillUpInvocation($aTeammateName, $aTeammate, $lolKingSource, $mappingService, $summonerService);
         $teammate->setMatchTeam($teammateTeam);
     }
 
@@ -131,7 +175,7 @@ foreach ($stats['matches'] as $aMatch) {
     $ennemyTeam = new Entity\MatchTeam;
     $ennemyTeam->setMatch($match);
     foreach ($aMatch['ennemies'] as $aEnnemyName => $aEnnemy) {
-        $ennemy = fillUpInvocation($aEnnemyName, $aEnnemy, $lolKingSource, $mappingService, $summonerService, $smiteSpell, $junglerPosition);
+        $ennemy = fillUpInvocation($aEnnemyName, $aEnnemy, $lolKingSource, $mappingService, $summonerService);
         $ennemy->setMatchTeam($ennemyTeam);
     }
 
@@ -144,6 +188,27 @@ foreach ($stats['matches'] as $aMatch) {
         $match->setLoser($teammateTeam);
     }
 
+    // Match type
+    $matchType = $mappingService->getMapper()->findOneOurs($lolKingSource, Entity\Mapping::TYPE_MATCH_TYPE, $aMatch['type']);
+    if ($matchType) {
+        $match->setMatchType($matchType);
+
+        // Match map
+        if ($matchType->getMap()) {
+            $match->setMap($matchType->getMap());
+        } else {
+            if (count($aMatch['teammates']) == 3) {
+                $match->setMap($twistedMap);
+            }
+        }
+    }
+
+    // Let's try to guess real positions of the players
+    if ($match->getMap() && $match->getMap()->getCode() == 'summoner-s-rift') {
+        guessTeamPositions($teammateTeam, $smiteSpell, $supportSpells, $positionsByKey);
+        guessTeamPositions($ennemyTeam,   $smiteSpell, $supportSpells, $positionsByKey);
+    }
+
     $hash = $matchService->calculateHash($match);
     $existingMatch = $matchService->getMapper()->findOneByHash($hash);
     $match->setHash($hash);
@@ -153,7 +218,7 @@ foreach ($stats['matches'] as $aMatch) {
         continue;
     }
 
-//    $em->persist($match);
+    $em->persist($match);
     $em->flush();
 
     $em->detach($match);
