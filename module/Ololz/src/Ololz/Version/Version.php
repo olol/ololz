c88a492ec5b5b74d53be86321bfa4e02f0f0fb30
Jérémy Huet <jeremy.huet@gmail.com>
2013-03-24 16:05:49 +0100

    Adding version to 0.1.0.

---
+++ module/Ololz/src/Ololz/Version/Version.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Ololz\Version;
+
+/**
+ * Class to store the version of Open LoL Ztatistics.
+ */
+final class Version
+{
+    /**
+     * Open LoL Ztatistics current version.
+     */
+    const VERSION = '0.1.0';
+}
