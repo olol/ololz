41a708a809866a4e83c034e0676adb12acf6bccf
Jeremy Huet <jeremy.huet@netapsys.fr>
2014-02-09 11:57:29 -0500

    Intermediate updaters

---
+++ module/Ololz/src/Ololz/Service/Updater/Match/Match.php
@@ -0,0 +1,14 @@
+<?php
+namespace Ololz\Service\Updater\Match;
+
+use Ololz\Service\Updater\Updater;
+
+/**
+ * Match updater
+ *
+ * @since   0.1
+ * @author  Jérémy Huet <jeremy.huet+olol@gmail.com>
+ * @link    https://github.com/olol/oLolZ
+ * @package Ololz
+ */
+abstract class Match extends Updater {}
